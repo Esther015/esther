b322aa7e732aac0eb3fc1325604cf7e9f7dccce2
Esther015 <estherlysgnanga@icloud.com>
2025-09-20 13:54:06 +0300

    Update kubsh.c

--- kubsh.c
+++ kubsh.c
@@ -13,3 +13,23 @@ int main(){
 
  return 0;
 }
+//ce aue j'ai copie
+include <stdio.h>
+#include <string.h>
+
+int main(){
+ char input[100];
+ printf("$ ");
+ while(1){
+
+  if( fgets(input, 100, stdin)== NULL);{
+  print("\nSortir par ctrl+d\n");
+  break;
+  }
+  input[strlen(input) -1]= '\0';
+
+  if(strcmp(input, "exit")==0){
+  printf(" exit from programm\n");
+  break;}
+  printf("%s: commamd not found\n", input):
+ }
